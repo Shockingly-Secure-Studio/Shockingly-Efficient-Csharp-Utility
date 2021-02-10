898663218a680178bdbfb881e18b1f820896b6a5
Giovanni Le Bail <giovanni.le-bail@epita.fr>
2021-02-10 14:39:02 +0100

    Add somestuff

---
+++ Shockingly Efficient C# Utility/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void ExitAPP()
+    {
+        Debug.Log("Exit App");
+        Application.Quit();
+    }
+    void ChangeScene(string NextScene){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
