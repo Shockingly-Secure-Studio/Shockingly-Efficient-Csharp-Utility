130f6bc4f5e7a5cd01bbf0cb413ccb81fcefe650
Helyosis <alois56200@gmail.com>
2021-02-12 22:25:09 +0100

    A

--- Shockingly Efficient C# Utility/Assets/Scripts/Utils.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Utils.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
@@ -81,5 +82,22 @@ public static class Utils
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             return Cmd(cmd);
         return Bash(cmd);
+    }
+
+    public static bool IsProgrammInstalled(string programm)
+    {
+        string res;
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            res = Cmd($"where {programm}").Split('\n')[0];
+        else
+            res = Bash($"which {programm}").Split('\n')[0];
+        
+        return new FileInfo(res).Exists;
+    }
+
+    public enum WebMethod
+    {
+        GET,
+        POST
     }
 }
