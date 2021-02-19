4c086cc81eb61de72e273bb2515ef5a6808e81a0
tudual <tudual.lefeuvre@epita.fr>
2021-02-19 11:26:09 +0100

    Scaner ip: makePing

--- Shockingly Efficient C# Utility/Assets/Scripts/Scan/Scan.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Scan/Scan.cs
@@ -10,16 +10,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Ping = System.Net.NetworkInformation.Ping;
 using System.Diagnostics;
+using System.Linq;
 using Debug = UnityEngine.Debug;
 
 public class Scan: MonoBehaviour
 {
-    private (string, string) ipRange = ("", "");
-    private List<IPAddress> ipList=new List<IPAddress>();//tableau [ip,[port]] 
-    private readonly object ipListLock = new object();
+    private List<IPAddress> ipList=new List<IPAddress>();//tableau [ip,[port]] (list couple Ip, port)
+    //private readonly object ipListLock = new object();
     public Scan()//TODO maske de sous réseau
     {
-        UnityEngine.Debug.Log(ReturnSubnetmask());
     }
 
     public static string GETLocalIp()
@@ -29,35 +28,64 @@ public class Scan: MonoBehaviour
         foreach (IPAddress ip in ipList)
             if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
             {
-                Debug.Log(ip);
+                Debug.Log("My ip:"+ip);
                 return ip.ToString();
             }
 
         return null;
     }
-    static public string ReturnSubnetmask()
+    public static (string,string) ReturnIpRange()
     {
         string ip = GETLocalIp();
         uint firstOctet = uint.Parse(ip.Split('.')[0]);
-        if (firstOctet <= 127)//classe A, 1.0.0.1 à 126.255.255.254
-            return "255.0.0.0";
-        if (firstOctet >= 128 && firstOctet <= 191)//classe B 	128.1.0.1 à 191.255.255.254
-            return "255.255.0.0";
-        if (firstOctet >= 192 && firstOctet <= 223)//classe C 192.0.1.1 à 223.255.254.254
-            return "255.255.255.0";//faire fonction ipv4
-        return "0.0.0.0";
-    }
-    
-    public async void makePing((string, string) ipRange)
-    {
-        IPAddress ip= IPAddress.Parse("127.0.0.1");//regarder le msque sous reseau trouver la plage d'aresse,
-        Task<IPAddress> pingTask = PingAsync(ip);
-        IPAddress newIp = await pingTask;
-        UnityEngine.Debug.Log(newIp);
-        if (newIp != null)
+        switch (firstOctet)
         {
-            ipList.Add(newIp);
+            case 10:
+                return ("10.0.0.0","10.255.255.255");//classe A, 10.0.0.0 à 10.255.255.255
+            case 172:
+                return ("172.16.0.0","172.31.255.255");//classe B 	172.16.0.0 à 172.31.255.255
+            case 192:
+                return ("192.168.1.0","192.168.255.255");//classe C  192.168.1.0 à 192.168.255.255
+            default:
+                return ("0.0.0.0","0.0.0.0"); 
         }
+    }
+    public async void makePing()
+    {
+        (string,string) ipRange = ReturnIpRange();
+        int[] ipStart = ipRange.Item1.Split('.').Select(int.Parse).ToArray();
+        int[] ipEnd = ipRange.Item2.Split('.').Select(int.Parse).ToArray();
+        IPAddress ip;
+        Task<IPAddress> pingTask;
+        var pingTaskList = new List<Task>();
+        for (var i = ipStart[3]; i < ipEnd[3]; i++)
+        {
+            for (var j = ipStart[2]; j < ipEnd[2]; j++)
+            {
+                for (var n = ipStart[1]; n < ipEnd[1]; n++)
+                {
+                    ip = IPAddress.Parse(ipStart[0] + "." + n + "." + j + "." + i);
+                    //pingTask = PingAsync(ip);
+                    pingTaskList.Add(PingAsync(ip));
+                }
+            }
+        }
+        //waite for all
+        while (pingTaskList.Count > 0)
+        {
+            //Task finishedTask = await Task.WhenAny(pingTaskList);
+            //pingTaskList.Remove(finishedTask);
+            Task<IPAddress> taskResult = await Task.WhenAny(pingTaskList) as Task<IPAddress>;
+            IPAddress newIp = await taskResult;
+            pingTaskList.Remove(taskResult);
+            //IPAddress newIp = await pingTask;
+            if (newIp != null)
+            {
+                UnityEngine.Debug.Log("New ip found"+newIp);
+                ipList.Add(newIp);
+            }
+        }
+        
     }
     
     //ping
