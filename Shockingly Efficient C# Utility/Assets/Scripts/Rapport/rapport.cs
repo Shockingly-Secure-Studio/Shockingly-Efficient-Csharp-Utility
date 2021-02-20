1797b1ac2b1641702f14eca35b59fa5981e55929
Giovanni Le Bail <giovanni.le-bail@epita.fr>
2021-02-20 19:11:11 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Shockingly-Secure-Studio/Shockingly-Efficient-Csharp-Utility into main""
    This reverts commit d7f9e3c9e5262643a948be878c67ceea3c4f11a6.

--- Shockingly Efficient C# Utility/Assets/Scripts/Rapport/rapport.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Rapport/rapport.cs
@@ -1,17 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Web;
 
-public class test : MonoBehaviour
+public class rapport : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //gameObject.AddComponent<Scan>().makePing(("127.0.0.1","127.0.0.1"));
-        //Scan test = new Scan();
-        Request test = new Request("haha",70,null,"fqsdf");
-        test.test();
+        
     }
 
     // Update is called once per frame
