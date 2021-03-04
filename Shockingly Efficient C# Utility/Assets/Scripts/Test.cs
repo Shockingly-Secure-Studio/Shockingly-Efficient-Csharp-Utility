83a5ecbea73942472573c7c0cb6deb628fd409d5
Helyosis <alois56200@gmail.com>
2021-03-04 13:07:59 +0100

    Finished linked web analysis and exploit
    Added a function to exploit Web Services.

--- Shockingly Efficient C# Utility/Assets/Scripts/Test.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Test.cs
@@ -1,38 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Renci.SshNet;
 using Service;
 using UnityEngine;
+using Machine = Machine.Machine;
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     async void Start()
     {
-        Debug.Log("Bonjour");
-        List<(string, int)> list = new List<(string, int)>();
-        List<string> url = new List<string>();
-        url.Add("http://localhost:8181");
-        List<string> map = web.map(list,url);
-        List<InputWebService> total = new List<InputWebService>();
-        
-        map.Add("http://localhost:8181");
-        
-        foreach (var link in map)
-        {
-            Debug.Log(link);
-            foreach (var inputWebService in await InputWebService.FromLink("127.0.0.1", 8181, link))
-            {
-                total.Add(inputWebService);
-            }
-        }
-
-        foreach (InputWebService inputWebService in total)
-        {
-            await inputWebService.Exploit(true);
-        }
-        
+        WebService ws = new WebService(new global::Machine.Machine("127.0.0.1"), "localhost", "127.0.0.1", 80);
+        Thread t = new Thread(ws.Exploit);
+        t.Start();
         /*
         Machine.Machine machine = new Machine.Machine("127.0.0.1");
         
