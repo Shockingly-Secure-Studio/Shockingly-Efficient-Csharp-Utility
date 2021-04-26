904e7e4b74aa43c5740426ce8f78e0ea9eec134f
Tudual Lefeuvre <tudual.lefeuvre@epita.fr>
2021-04-26 09:43:10 +0200

    rapport.cs

--- Shockingly Efficient C# Utility/Assets/Scripts/Rapport/rapport.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Rapport/rapport.cs
@@ -13,13 +13,22 @@ using UnityEngine;
 public class rapport: MonoBehaviour
 {
     List<SaveScan.Device> devicesList = SaveScan.LoadJson("scanPort");
-    public void test()
+
+    public void NewDocument()
     {
         System.IO.FileStream fs = new FileStream(@".\report.pdf", FileMode.Create);
         Document document = new Document(PageSize.A4, 25, 25, 30, 30);  
         PdfWriter writer = PdfWriter.GetInstance(document, fs);  
         document.Open();
-        //date du document
+        Header(document,writer);
+        WriteInfo(document,writer);
+        document.Close(); 
+        writer.Close();
+        fs.Close();
+    }
+
+    private void Header(Document document,PdfWriter writer)
+    {
         Rectangle page = document.PageSize;
         PdfPTable head = new PdfPTable(1);
         head.TotalWidth = page.Width;
@@ -38,25 +47,76 @@ public class rapport: MonoBehaviour
             page.Height - document.TopMargin + head.TotalHeight + 20,
             writer.DirectContent 
         );
+    }
 
-        var font = iTextSharp.text.Font.FontFamily.TIMES_ROMAN;
-        phrase = new Phrase("Rapport de l'analyse",new iTextSharp.text.Font(font,30));
+    private void NewTitle(Document document,string text,iTextSharp.text.Font.FontFamily font,int size)
+    {
+        var phrase = new Phrase(text,new iTextSharp.text.Font(font,size));
         var title = new Paragraph(phrase);
         title.Alignment = Element.ALIGN_CENTER;
         document.Add(title);
+    }
 
-        iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(@"./img.png");
+    private void NewImage(Document document,string path)
+    {
+        iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(path);
         img.ScaleToFit(140f, 120f);
         img.SpacingBefore = 10f;
         img.SpacingAfter = 1f;
         img.Alignment = Element.ALIGN_CENTER;
         document.Add(img);
-            
+    }
+
+    private PdfPTable MakeTable(Document document, int nbcol,string colInfo, string colW)
+    {
+        string[] col =new string[nbcol];
+        var n = 0;
+        foreach (var colName in colInfo.Split(','))
+        {
+            col[n] = colName;
+            n++;
+        }
+        
+        PdfPTable table = new PdfPTable(nbcol);
+        table.WidthPercentage = 100;
+        var widths = new Single[nbcol];
+        n = 0;
+        foreach (var width in colW.Split(','))
+        {
+            widths[n] = int.Parse(width);
+            n++;
+        }
+        table.SetWidths(widths);
+        table.SpacingBefore = 10;
+        
+        foreach (var t in col)
+        {
+            PdfPCell cell = new PdfPCell(new Phrase(t));
+            cell.BackgroundColor = new BaseColor(204, 204, 204);
+            table.AddCell(cell);
+        }
+        return table;
+    }
+
+    private void AddLine(ref PdfPTable table,string info)
+    {
+        string[] infos = info.Split(',');
+        int nbC = table.NumberOfColumns;
+        for (var i = 0; i < nbC; i++)
+        {
+            table.AddCell(infos[i%nbC]);
+        }
+    }
+    private void WriteInfo(Document document,PdfWriter writer)
+    {
+        var font = iTextSharp.text.Font.FontFamily.TIMES_ROMAN;
+        NewTitle(document,"Rapport de l'analyse",font,30);
+        NewImage(document,@"./img.png");
+        var newTable=MakeTable(document,6,"IP,Severity level,Number of flaw,RCE,SQLi,XSS","7,3,5,3,3,3");
+        
         foreach (var device in devicesList)
         {
-            string info = "The machine " + device.hostName + "(" + device.IP + ")" + "has been scaned and we found " +
-                          device.nbOfSFlaw
-                          + " flaws, the security level is " + device.severityLevel+".\n";
+            string info = $"{device.IP},{device.nbOfSFlaw},{device.severityLevel}";
             DirectoryInfo deviceDirectoryInfo = new DirectoryInfo(@".\Results\"+device.IP+@"\");
             var dirList=deviceDirectoryInfo.EnumerateDirectories();
             foreach (var dir in dirList)
@@ -66,18 +126,17 @@ public class rapport: MonoBehaviour
                 {
                     string json = File.ReadAllText(path);
                     List<AccessPoint> accessPoints = JsonConvert.DeserializeObject<ServiceResult>(json).AccessPoints;
+                    int[] nbF = new int[3];
                     for(var i=0;i<accessPoints.Count;i++)
                     {
-                        info += "\tFlaw "+i+": "+accessPoints[i].Type.ToString()+".\n";
+                        nbF[(int) accessPoints[i].Type] += 1;
                     }
+                    info += $",{nbF[0]},{nbF[1]},{nbF[2]}";
                 }
             }
-            document.Add(new Paragraph(info,new iTextSharp.text.Font(font,12)));
+            AddLine(ref newTable,info);
         }
-        
-        document.Close(); 
-        writer.Close();
-        fs.Close();
+        document.Add(newTable);
 
     }
    
