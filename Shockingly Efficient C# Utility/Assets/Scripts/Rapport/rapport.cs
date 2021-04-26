d89e4b10a20321536c334c71cca6b2ce0ff16f3c
Tudual Lefeuvre <tudual.lefeuvre@epita.fr>
2021-04-26 10:27:52 +0200

    fin rapport.cs

--- Shockingly Efficient C# Utility/Assets/Scripts/Rapport/rapport.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Rapport/rapport.cs
@@ -109,11 +109,12 @@ public class rapport: MonoBehaviour
     }
     private void WriteInfo(Document document,PdfWriter writer)
     {
-        var font = iTextSharp.text.Font.FontFamily.TIMES_ROMAN;
-        NewTitle(document,"Rapport de l'analyse",font,30);
+        var fontFamily = iTextSharp.text.Font.FontFamily.TIMES_ROMAN;
+        NewTitle(document,"Rapport de l'analyse",fontFamily,30);
         NewImage(document,@"./img.png");
-        var newTable=MakeTable(document,6,"IP,Severity level,Number of flaw,RCE,SQLi,XSS","7,3,5,3,3,3");
-        
+        NewTitle(document,"Tableaux récapitulatif",fontFamily,15);
+        var newTable=MakeTable(document,6,"IP,Niveau de vulnérabilité,Nombre total de failles,RCE,SQLi,XSS","7,5,5,3,3,3");
+        List<AccessPointType> flaws = new List<AccessPointType>();
         foreach (var device in devicesList)
         {
             string info = $"{device.IP},{device.nbOfSFlaw},{device.severityLevel}";
@@ -130,6 +131,8 @@ public class rapport: MonoBehaviour
                     for(var i=0;i<accessPoints.Count;i++)
                     {
                         nbF[(int) accessPoints[i].Type] += 1;
+                        if(!flaws.Contains(accessPoints[i].Type))
+                            flaws.Add(accessPoints[i].Type);
                     }
                     info += $",{nbF[0]},{nbF[1]},{nbF[2]}";
                 }
@@ -137,6 +140,44 @@ public class rapport: MonoBehaviour
             AddLine(ref newTable,info);
         }
         document.Add(newTable);
+        foreach (var flaw in flaws)
+        {
+            switch (flaw)
+            {
+                case AccessPointType.RCE:
+                    NewTitle(document,"RCE",fontFamily,13);
+                    document.Add(new Paragraph("\tRCE ou remonte commande injection, ce sont des failles qui" +
+                                               " permettent à l’utilisateur d’exécuter des commandes, pour éviter " +
+                                               "ce type de faille il faut vérifier  les requêtes de l’utilisateur, " +
+                                               "par exemple en vérifiant les symboles utilisées. \n", 
+                        new iTextSharp.text.Font(fontFamily, 12)));
+                    break;
+                case AccessPointType.SQLi:
+                    NewTitle(document,"SQLi",fontFamily,13);
+                    document.Add(new Paragraph(
+                        "\tLes SQLi, aussi appelé injection SQL est un type de faille qui a pour but interagir avec une base de " +
+                        "données, pour cela on injecte un morceau malveillant de requête SQL dans une requête SQL qui" +
+                        " va par exemple vérifier un mot de passe. Ce type de faille peux permettre par exemple de " +
+                        "récupérer tous les mot de passe et les nom d’utilisateurs.Voici une ressource pour vous " +
+                        "protéger contre les injections sql\nhttps://cheatsheetseries.owasp.org/cheatsheets/SQL_Inject" +
+                        "ion_Prevention_Cheat_Sheet.html\n",
+                        new iTextSharp.text.Font(fontFamily, 12)));
+                    break;
+                case AccessPointType.XSS:
+                    NewTitle(document,"XSS",fontFamily,13);
+                    document.Add(new Paragraph(
+                        "\tLes failles XSS sont des failles liées aux différents point d’entrés du site web, per exemple " +
+                        "en laissant un commentaire l’utilisateur peux tenter d’injecter du code malveillant, si votre serveur " +
+                        "ne vérifie pas ce que rentre l’utilisateur il va renvoyer le code qui va être interpréter par les " +
+                        "navigateurs des autres utilisateur qui visiterons la page. Ce type d’attaques peux permettre de " +
+                        "voler les cookies, si un personne vole un cookie d’un administrateur il peux élever ses privilèges" +
+                        " et accéder à des informations sensible. Il peux aussi par exemple rediriger les utilisateurs sur " +
+                        "un page malveillantes. Voici une ressouce pour vous protéger contre les XSS\nhttps://cheatsheet" +
+                        "series.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.html#defense-against-xss\n",
+                        new iTextSharp.text.Font(fontFamily, 12)));
+                    break;
+            }
+        }
 
     }
    
