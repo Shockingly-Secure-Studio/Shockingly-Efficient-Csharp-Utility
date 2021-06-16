7d44b8920f113cba9f5c5c0599cab428f6621290
Aloïs Colléaux-Le Chêne <alois56200@gmail.com>
2021-06-16 17:35:06 +0200

    Added DNSClient library. To devellop DNS exploitation for later

--- Shockingly Efficient C# Utility/Assets/Scripts/Service/DNSService.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Service/DNSService.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using DnsClient;
 
 namespace Service
 {
     public class DNSService : UdpService
     {
-
         /// <summary>
         /// DNS Service is UDP-based
         /// </summary>
@@ -14,11 +15,13 @@ namespace Service
         /// <param name="port"></param>
         public DNSService(Machine.Machine machine, int port) : base(machine, port)
         {
+            var lookup = new LookupClient();
+            var result = lookup.QueryAsync("google.com", QueryType.A);
+
+            var record = result.Answers.ARecords().FirstOrDefault();
+            var ip = record?.Address;
         }
 
-        public void MakeZoneTransfer()
-        {
-            
-        }
+        
     }
 }
--- Shockingly Efficient C# Utility/Assets/Scripts/Test.cs
+++ Shockingly Efficient C# Utility/Assets/Scripts/Test.cs
@@ -13,12 +13,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     async void Start()
     {
-        string ip = "192.168.56.102";
-        Machine.Machine machine = new Machine.Machine(ip);
-        WebService ws = new WebService(
-            machine, 80, ip
-        );
-        await ws.Exploit();
+        DNSService dnsService = new DNSService();
     }
 
     // Update is called once per frame
